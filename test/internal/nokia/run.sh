4ee989990a2c55d4dfe0d692ccba0240b3075fa7
Aaron Steele <aasteele@cisco.com>
2019-03-04 13:59:10 -0800

    Fixing directory error in test

--- test/internal/nokia/run.sh
+++ test/internal/nokia/run.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-export PYTHONPATH=../../../python/nfvo_solcon_tosca
+root=../../..
+export PYTHONPATH=$root/python/nfvo_solcon_tosca
 
 # Run the four examples that the program shouldn't *at least* crash with
-tosca=../../../solcon.py
-config_dir=../../../config
+tosca=$root/solcon.py
+config_dir=$root/config
+output_dir=$root/outputs
 config_tosca=$config_dir/config-nokia.toml
 config_sol6=$config_dir/config-sol6.toml
 
 one=../../../examples/VNFD-CMRepo-for-cisco.yaml
 two=../../../examples/VNFD-CSD-for-cisco.yaml
 echo Run $one...
-/usr/local/bin/python3 $tosca -f $one -o ..././../outputs/output_CMRepo.json -c $config_tosca -s $config_sol6
+/usr/local/bin/python3 $tosca -f $one -o $output_dir/output_CMRepo.json -c $config_tosca -s $config_sol6
 echo Run $two...
-/usr/local/bin/python3 $tosca -f $two -o ../../../outputs/output_CSD.json -c $config_tosca -s $config_sol6
+/usr/local/bin/python3 $tosca -f $two -o $output_dir/output_CSD.json -c $config_tosca -s $config_sol6
