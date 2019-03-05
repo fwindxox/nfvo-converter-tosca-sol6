94e31a8c1f68862a9dadb271c8173fc5d935da4f
Aaron Steele <aasteele@cisco.com>
2019-03-05 11:52:16 -0800

    Changing some script stuff

--- test/internal/nokia/run.sh
+++ test/internal/nokia/run.sh
@@ -12,6 +12,6 @@ config_sol6=$config_dir/config-sol6.toml
 one=../../../examples/VNFD-CMRepo-for-cisco.yaml
 two=../../../examples/VNFD-CSD-for-cisco.yaml
 echo Run $one...
-/usr/local/bin/python3 $tosca -f $one -o $output_dir/output_CMRepo.json -c $config_tosca -s $config_sol6
+python3 $tosca -f $one -o $output_dir/output_CMRepo.json -c $config_tosca -s $config_sol6
 echo Run $two...
-/usr/local/bin/python3 $tosca -f $two -o $output_dir/output_CSD.json -c $config_tosca -s $config_sol6
+python3 $tosca -f $two -o $output_dir/output_CSD.json -c $config_tosca -s $config_sol6
