e2193c458cf232cb014f411d1c28f50f8e1c6515
Aaron Steele <aasteele@cisco.com>
2019-05-07 10:43:06 -0700

    Adding more fields to VNF

--- src/sol6_writer/Sol6Writer.py
+++ src/sol6_writer/Sol6Writer.py
@@ -17,12 +17,23 @@ class Sol6Writer:
         nfv = etree.SubElement(data, "nfv", nsmap={None: "urn:etsi:nfv:yang:etsi-nfv-descriptors"})
         vnfd = etree.SubElement(nfv, "vnfd")
 
-        add_child(vnfd, "id", self.model.VNF.descriptor_id)
+        VNF = self.model.VNF
+
+        add_child(vnfd, "id", VNF.descriptor_id)
+        add_child(vnfd, "provider", VNF.provider)
+        add_child(vnfd, "product", VNF.product_name)
+        add_child(vnfd, "software-version", VNF.software_version)
+        add_child(vnfd, "version", VNF.descriptor_version)
+        add_child(vnfd, "product-info-name", VNF.product_info_name)
+        add_child(vnfd, "product-info-description", VNF.product_info_description)
+        add_child(vnfd, "vnfm-info", VNF.vnfm_info)
 
         with open(file_name, 'wb') as f:
             f.write(etree.tostring(data, pretty_print=True, xml_declaration=True))
 
 
 def add_child(parent, name, value=""):
+    if not value and value is not 0:
+        return
     temp = etree.SubElement(parent, name)
-    temp.text = value
+    temp.text = str(value)
